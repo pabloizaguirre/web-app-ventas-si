86ae3338466238c9685f5e726fef3510a84032f8
GianLLZO <115785401+GianLLZO@users.noreply.github.com>
2022-11-17 15:01:06 +0100

    Add files via upload

---
+++ database/updInventoryAndCustomer.sql
@@ -0,0 +1,36 @@
+
+CREATE OR REPLACE FUNCTION updInventoryAndCustomer()
+RETURNS TRIGGER AS $$
+DECLARE
+    rec RECORD;
+BEGIN
+
+    -- Updating products (inventory) information and customer's balance information
+    FOR rec IN
+        SELECT products.prod_id as prod_id, quantity, products.price as price, customerid 
+        FROM orderdetail, orders, products
+        WHERE OLD.orderid=orderdetail.orderid 
+        AND products.prod_id=orderdetail.prod_id
+    LOOP
+        UPDATE products
+        SET stock=stock-rec.quantity,
+            sales=sales+rec.quantity
+        WHERE prod_id=rec.prod_id;
+        
+        UPDATE customer
+        SET balance = balance-rec2.price
+        WHERE customerid=rec2.customerid;
+
+    END LOOP;
+   
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER updInventoryAndCustomer
+AFTER UPDATE
+ON orders
+FOR EACH ROW
+WHEN (OLD.status IS DISTINCT FROM NEW.status)
+EXECUTE PROCEDURE updInventoryAndCustomer();
